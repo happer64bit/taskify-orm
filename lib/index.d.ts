51dd6cc0974eb04da21836530b161cfd505b8955
Happer World <happer64bit@gmail.com>
2023-05-06 13:12:34 +0630

    Updated Name

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,32 @@
+import sqlite3 from 'sqlite3';
+
+type Task = {
+    id: number;
+    title: string;
+    isChecked: boolean;
+    created_at: string;
+};
+
 declare class TaskOrm {
-    private databaseName;
-    private db;
-    constructor(file: string);
-    query(query: string, params: any[]): void;
-    createTaskTable(name: string): void;
-    createKey(tableName: string, title: string): void;
-    deleteKey(tableName: string, id: string): void;
-    getAllTable(): Promise<string[]>;
-    fetchAllInTable(tableName: string): Promise<any[]>;
-    setChecked(tableName: string, id: string, isChecked: boolean): void;
+    private db: sqlite3.Database;
+
+    constructor(databaseName: string);
+
+    initTable(): void;
+
+    createTask(title: string, isChecked?: boolean): Promise<Task>;
+
+    getTask(id: number): Promise<Task>;
+
+    updateTask(id: number, title?: string, isChecked?: boolean): Promise<Task>;
+
+    deleteTask(id: number): Promise<void>;
+
+    getAllTasks(): Promise<Task[]>;
+
+    run(query: string, params?: any[]): Promise<sqlite3.RunResult>;
+
+    all(query: string, params?: any[]): Promise<any[]>;
 }
 
 export default TaskOrm;
